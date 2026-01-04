bb19950e14b684d52b848094bede31b37b07aab9
Yashdeep Tandon <yashdeeptandon007@gmail.com>
2026-01-04 23:27:53 +0530

    done till routing

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const http = require('http');
-
+const url = require('url');
 ////////////////////////////////
 // FILE
 
@@ -23,16 +23,70 @@ const textOut = `This is what we know about the avocado: ${textIn}. \n Created o
 fs.writeFileSync('./txt/output.txt', textOut);
 console.log('File Written!');
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
 
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+  return output;
+};
 
 
 //////////////////////////
 // SERVER
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+
+
 const server = http.createServer((req, res) =>{
-  res.end('Hello from the server.')
+  const pathName = req.url;
+
+  // OVERVIEW
+  if(pathName === '/overview' || pathName === '/'){
+
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+    const cardsHtml = dataObj.map(item => replaceTemplate(tempCard, item)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+  }else if(pathName === '/product'){
+    res.end(tempProduct);
+  }else if(pathName === '/api'){
+    // API
+    res.writeHead(200, {
+        'content-type': 'application/json'
+      })
+      res.end(data);
+  }else{
+    // NOT FOUND
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'yashdeep-tandon',
+    })
+
+    res.end('<h1>Page not found!</h1>')
+  }
+
 });
 
 server.listen(8000, '127.0.0.1', () =>{
   console.log('Listening to the requests on port 8000');
-})
+});
+
+
+// ROUTING
+
